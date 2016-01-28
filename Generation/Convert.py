0728566345b87887980f922814f5dd4a7790c013
Gallactide <mrdgall@gmail.com>
2016-01-28 22:26:26 +0100

    Generation V2: Now with Batch Processing

--- Generation/Convert.py
+++ Generation/Convert.py
@@ -11,11 +11,17 @@ Game took Game state turn 9 turns, had        0 timeouts
 ('maps/3planets/map1.txt', 'BerendHillBot.py', 'AdaptiveBot.py', -1, '9')
 """
 
-def AddToSheet(results, sheets, coords, columns):
+def AddToSheet(filename, sheets, coords, columns):
+    file = open(filename).read().split("\n")
+
+    results = sorted([extract(i) for i in range(0,len(file)-2,3)], key=lambda x: x[3])
+    maps = sorted([(games[i][0], games[i][-2]+games[i+1][-2]+games[i+2][-2]+games[i+3][-2]+games[i+4][-2]) for i in range(0, len(games), 5)], key=lambda x: x[1], reverse=True)
+    print("Processing file {0}: ".format(filename))
     print("Processing {0} Results...".format(len(games)))
     t = time.time()
     wb = load_workbook("Template.xlsx")
     last_wb = "Template.xlsx"
+
     for data in results:
         print(data)
         if (last_wb!=sheets[data[2]]):
@@ -23,7 +29,7 @@ def AddToSheet(results, sheets, coords, columns):
             last_wb = sheets[data[2]]
             wb = load_workbook(sheets[data[2]])
             ws = wb.active
-        column = columns[sys.argv[1].split("_")[2]]+int(sys.argv[1].split(".")[0][-1])
+        column = columns[filename.split("_")[2]]+int(filename.split(".")[0][-1])
         cell = string.ascii_uppercase[column]+str(coords[data[0]])
         ws[cell] = data[-1]
         if (data[-2]==-1):
@@ -37,6 +43,7 @@ def extract(index):
     player = lines[0].split(" vs ")[0].split(" ")[-1]
     enemy = lines[0].split(" vs ")[1].split(" ")[0]
     map = lines[0].split(" vs ")[1].split(" ")[-1]
+
     if "Draw!" in lines[1]:
         state = 0
     else:
@@ -50,14 +57,11 @@ def extract(index):
 file = open(sys.argv[1]).read().split("\n")
 print(len(file))
 
-games = sorted([extract(i) for i in range(0,len(file)-2,3)], key=lambda x: x[3])
-
-maps = sorted([(games[i][0], games[i][-2]+games[i+1][-2]+games[i+2][-2]+games[i+3][-2]+games[i+4][-2]) for i in range(0, len(games), 5)], key=lambda x: x[1], reverse=True)
-
 rows = dict(zip(['maps/3planets/map1.txt','maps/3planets/map2.txt','maps/3planets/map3.txt','maps/4planets/map1.txt','maps/4planets/map2.txt','maps/4planets/map3.txt','maps/5planets/map1.txt','maps/5planets/map2.txt','maps/5planets/map3.txt','maps/6planets/map1.txt','maps/6planets/map2.txt','maps/6planets/map3.txt','maps/7planets/map1.txt','maps/7planets/map2.txt','maps/7planets/map3.txt','maps/8planets/map1.txt','maps/8planets/map2.txt','maps/8planets/map3.txt'], range(4,22)))
 
 columns = {"hill": 1, "minmax": 6, "alphabeta":11}
 
 sheets = {'BullyBot.py':"Bully.xlsx", 'RandomBot.py':"Random.xlsx", 'EmptyBot.py':"Empty.xlsx", 'LookaheadBot.py':"Lookahead.xlsx", 'AdaptiveBot.py':"Adaptive.xlsx"}
 
-AddToSheet(games, sheets, rows, columns)
+for i in argv[1:]:
+    AddToSheet(i, sheets, rows, columns)
